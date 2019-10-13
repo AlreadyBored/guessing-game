aafcf4e3eb93265599a635378d6241cc66be2b8a
AlreadyBored <shilaums@gmail.com>
2019-10-13 17:20:36 +0300

    init: start guessing-game task & all tests passed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this._min = null;
+        this._max = null;
+
+        this._currGuess = null;
+    }
 
     setRange(min, max) {
-
+        this._min = min;
+        this._max = max;
     }
 
     guess() {
-
+        this._currGuess = Math.round((this._min + this._max) / 2);
+        return this._currGuess;
     }
 
     lower() {
-
+        this._max = this._currGuess;
     }
 
     greater() {
-
+        this._min = this._currGuess;
     }
 }
 
